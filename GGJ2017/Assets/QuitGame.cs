2a4bf69e472257746e7d477337e8d441a9e96e81
Poconnor2018 <Patrick.m.oconnor2012@gmail.com>
2017-01-21 19:13:45 -0500

    Completed main menu and added title art

--- GGJ2017/Assets/QuitGame.cs
+++ GGJ2017/Assets/QuitGame.cs
@@ -3,14 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuitGame : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void press()
+    {
+        Application.Quit();
+    }
 }
